d698b10fab6676a8080747a9e22d4646f94e4b8e
martony38 <martony@gmail.com>
2017-12-28 16:23:25 -0500

    Improve error handling for wikipedia api calls

--- js/wikipedia_api.js
+++ js/wikipedia_api.js
@@ -1,28 +1,22 @@
 class WikipediaAPI {
   constructor() {
     this.getArticles = () => {
-      // Use a timeout to disply error message if API call fail.
-      const errorTimeout = setTimeout(() => {
-        spotViewModel.addMessage('Error: Wikipedia Articles Could Not Be Loaded.', 'alert-danger');
-      }, 10000);
-
       $.ajax({
         url: 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=' + spotViewModel.currentSpot().title,
-        dataType: 'jsonp',
-        success(data) {
-          let articles = [];
-          $.each(data[1], (index, article) => {
-              articles.push({
-                title: article,
-                description: data[2][index],
-                url: data[3][index]
-              });
-          });
-          spotViewModel.addInfoToCurrentSpot('articles', articles);
-
-          // Cancel timeout for error message.
-          clearTimeout(errorTimeout);
-        }
+        dataType: 'jsonp'
+      }).done(data => {
+        let articles = [];
+        $.each(data[1], (index, article) => {
+            articles.push({
+              title: article,
+              description: data[2][index],
+              url: data[3][index]
+            });
+        });
+        spotViewModel.addInfoToCurrentSpot('articles', articles);
+      }).fail(() => {
+        // Use a timeout to disply error message if API call fail.
+        spotViewModel.addMessage('Error: Wikipedia Articles Could Not Be Loaded.', 'alert-danger');
       });
     };
   }
